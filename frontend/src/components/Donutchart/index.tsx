e7e053c717ffcc4bec657ebac6a71233e0793d20
maria cicilia <mariacicilia22@hotmail.com>
2021-09-08 18:55:38 -0300

    security config

--- frontend/src/components/Donutchart/index.tsx
+++ frontend/src/components/Donutchart/index.tsx
@@ -3,8 +3,8 @@ import Chart from 'react-apexcharts';
 const Donutschart = () => {
 
     const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        series: [500000, 499928, 444867, 220426, 473088],
+        labels: ['Maria Cicilia', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
     
     const options = {
